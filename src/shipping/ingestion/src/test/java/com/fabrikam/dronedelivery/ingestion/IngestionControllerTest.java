838d63e4f423bbd7b5b0a4e08838c0397dab0af1
cfarre <cfarre@microsoft.com>
2018-02-08 12:19:24 -0800

    added integration tests+profile

--- src/shipping/ingestion/src/test/java/com/fabrikam/dronedelivery/ingestion/IngestionControllerTest.java
+++ src/shipping/ingestion/src/test/java/com/fabrikam/dronedelivery/ingestion/IngestionControllerTest.java
@@ -22,9 +22,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-//import org.springframework.web.context.WebApplicationContext;
-//import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import com.fabrikam.dronedelivery.ingestion.configuration.ApplicationProperties;
