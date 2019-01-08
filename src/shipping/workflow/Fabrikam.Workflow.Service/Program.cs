37dfc4bef3b19fc3ddec2a3bac8f099aeac8b174
Fernando Simonazzi <v-fesimo@microsoft.com>
2019-01-08 16:57:52 +0000

    Merged PR 623: add skeleton workflow service
    add skeleton workflow service
    
    Related work items: #8289

---
+++ src/shipping/workflow/Fabrikam.Workflow.Service/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Formatting.Compact;
+using System.Threading.Tasks;
+
+namespace Fabrikam.Workflow.Service
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await CreateHostBuilder(args).Build().RunAsync();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return new HostBuilder()
+                .ConfigureAppConfiguration((environment, builder) =>
+                {
+                    builder.AddEnvironmentVariables();
+
+                    var buildConfig = builder.Build();
+                    if (buildConfig["CONFIGURATION_FOLDER"] is var configurationFolder && !string.IsNullOrEmpty(configurationFolder))
+                    {
+                        builder.AddKeyPerFile(configurationFolder, false);
+                    }
+                })
+                .ConfigureLogging(builder =>
+                {
+                    var serilogBuilder = new LoggerConfiguration()
+                        //.ReadFrom.Configuration(Configuration)
+                        //.Enrich.With(new CorrelationLogEventEnricher(httpContextAccessor, Configuration["Logging:CorrelationHeaderKey"]))
+                        .WriteTo.Console(new CompactJsonFormatter());
+
+                    builder.AddSerilog(serilogBuilder.CreateLogger(), true);
+                })
+                .ConfigureServices(services =>
+                {
+                    services.AddHostedService<WorkflowService>();
+                })
+                .UseConsoleLifetime();
+        }
+    }
+}
---
+++ src/shipping/workflow/Fabrikam.Workflow.Service/WorkflowService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fabrikam.Workflow.Service
+{
+    internal class WorkflowService : IHostedService
+    {
+        private readonly ILogger<WorkflowService> _logger;
+        private Timer _timer;
+
+        public WorkflowService(ILogger<WorkflowService> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _timer = new Timer(c =>
+                {
+                    _logger.LogInformation("Processing...");
+                }, null,
+                TimeSpan.Zero,
+                TimeSpan.FromSeconds(30));
+
+
+            _logger.LogInformation("Started");
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _timer.Change(Timeout.InfiniteTimeSpan, Timeout.InfiniteTimeSpan);
+            _timer.Dispose();
+
+            _logger.LogInformation("Stopped");
+            return Task.CompletedTask;
+        }
+    }
+}
