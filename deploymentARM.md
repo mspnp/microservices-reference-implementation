483c640eb24cca1cd6d8642cdc3d7913c9133db4
Fernando Simonazzi <v-fesimo@microsoft.com>
2019-03-13 14:14:08 +0000

    Merged PR 688: Update dev dependencies for package service
    update dev dependencies, migrate gulp script to match
    
    Related work items: #8562

--- deploymentARM.md
+++ deploymentARM.md
@@ -335,7 +335,7 @@ helm install stable/nginx-ingress --name nginx-ingress --namespace ingress-contr
 # Obtain the load balancer ip address and assign a domain name
 until export INGRESS_LOAD_BALANCER_IP=$(kubectl get services/nginx-ingress-controller -n ingress-controllers -o jsonpath="{.status.loadBalancer.ingress[0].ip}" 2> /dev/null) && test -n "$INGRESS_LOAD_BALANCER_IP"; do echo "Waiting for load balancer deployment" && sleep 20; done
 export INGRESS_LOAD_BALANCER_IP_ID=$(az network public-ip list --query "[?ipAddress!=null]|[?contains(ipAddress, '$INGRESS_LOAD_BALANCER_IP')].[id]" --output tsv)
-export EXTERNAL_INGEST_DNS_NAME="${UNIQUE_APP_NAME_PREFIX}-ingest"
+export EXTERNAL_INGEST_DNS_NAME="${RESOURCE_GROUP}-ingest"
 export EXTERNAL_INGEST_FQDN=$(az network public-ip update --ids $INGRESS_LOAD_BALANCER_IP_ID --dns-name $EXTERNAL_INGEST_DNS_NAME --query "dnsSettings.fqdn" --output tsv)
 INGRESS_TLS_SECRET_NAME=ingestion-ingress-tls
 
--- src/shipping/package/gulpfile.js
+++ src/shipping/package/gulpfile.js
@@ -4,16 +4,16 @@ var nodemon = require('gulp-nodemon');
 var del = require('del');
 var mocha = require('gulp-mocha');
 
-gulp.task('set-env', function() {
+gulp.task('set-env', gulp.series(function(done) {
     process.env.CONNECTION_STRING = "mongodb://packagedb:27017/local";
     process.env.COLLECTION_NAME = "packages";
     process.env.CORRELATION_HEADER = "x-b3-traceid";
     process.env.LOG_LEVEL = "debug"
-    return;
-});
+    done();
+}));
 
 // Generate types that can be used when working with api definitions
-gulp.task ('create-api-classes', function() {
+gulp.task ('create-api-classes', gulp.series(function() {
 
     let generated = "// GENERATED FILE - DO NOT EDIT\n\n";
 
@@ -47,9 +47,14 @@ gulp.task ('create-api-classes', function() {
             return console.log(err);
         }
     });
-});
+}));
 
-gulp.task('build', ['clean'], function () {
+// delete all build output files
+gulp.task('clean', gulp.series(function (done) {
+    return del(['.bin/'], done);
+ }));
+ 
+gulp.task('build', gulp.series('clean', function () {
     gulp.src('**/*.json', {'cwd':'app'})
         .pipe(gulp.dest('.bin/app'));
 
@@ -59,34 +64,29 @@ gulp.task('build', ['clean'], function () {
         .pipe(tsProject());
 
     return tsResult.js.pipe(gulp.dest('.bin/app'));
-});
+}));
 
-gulp.task('serve', ['build', 'set-env'], function (cb) {
+gulp.task('serve', gulp.series('build', 'set-env', function (cb) {
     return nodemon({
         script: '.bin/app/main.js',
         ext: 'ts json',
         watch: 'app',
         env: { "PORT": 80 },
         tasks: ["build"],
-        env: { 'NODE_ENV': 'dev' }
+        env: { 'NODE_ENV': 'development' }
     })
-});
+}));
 
-// delete all build output files
-gulp.task('clean', function (done) {
-   return del(['.bin/'], done);
-});
-
-gulp.task('build-int-test', ['build'], function  () {
+gulp.task('build-int-test', gulp.series('build', function  () {
     var tsProject = ts.createProject('tsconfig.json');
 
     var tsResult = gulp.src("test/**/*.ts", {'base':'.'})
         .pipe(tsProject());
 
     return tsResult.js.pipe(gulp.dest('.bin'));
-});
+}));
 
-gulp.task('int-test', ['build-int-test', 'set-env'], function() {
-    gulp.src("test/**/*.js", { cwd: '.bin' })
-       .pipe(mocha({ reporter: 'list' }));
-});
+gulp.task('int-test', gulp.series('build-int-test', 'set-env', function() {
+    return gulp.src("test/**/*.js", { cwd: '.bin' })
+       .pipe(mocha({ reporter: 'list', exit: true }));
+}));
