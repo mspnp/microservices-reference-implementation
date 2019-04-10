26b9aa1a9873f3ba6c906dee3b7d1f63c7ce88b3
Fernando Antivero <v-fean@microsoft.com>
2019-04-10 16:08:11 +0000

    Merged PR 725: improve app properties testing
    improve app properties testing

--- src/shipping/ingestion/src/test/java/com/fabrikam/dronedelivery/ingestion/ApplicationPropertiesTest.java
+++ src/shipping/ingestion/src/test/java/com/fabrikam/dronedelivery/ingestion/ApplicationPropertiesTest.java
@@ -1,48 +1,26 @@
 package com.fabrikam.dronedelivery.ingestion;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.times;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import com.fabrikam.dronedelivery.ingestion.configuration.ApplicationProperties;
 
 public class ApplicationPropertiesTest {
-	
-	public class Environment {
-	    public String getenv(String envName) {
-	        return System.getenv(envName); 
-	    }
-	}
-	
-	
-	 @Configuration
-	    public static class Config {
-	    @Bean
-	        public static PropertySourcesPlaceholderConfigurer propertyConfigurer() {
-	            return new PropertySourcesPlaceholderConfigurer();
-	        }
-	    }
-		  
-	  
-	@Before
-	public void setUp() throws Exception {			
-		MockitoAnnotations.initMocks(this);		    
-	}
-
 	@Test
-	public void canGetPropertiesfromEnvVariables() {
-		Environment systemMock = Mockito.mock(Environment.class);
-		Mockito.when(systemMock.getenv("ENV_QUEUE_NAME")).thenReturn("variableMock");
+	public void canSetPropertyValuesUsedByClientPoolImpl_thenGetPropertyValues() {
+		// Arrange
 		ApplicationProperties appProps = new ApplicationProperties();
-		assertEquals(systemMock.getenv(appProps.getEnvQueueName()),"variableMock");
-		Mockito.verify(systemMock, times(1))
-		 .getenv(appProps.getEnvQueueName());		
-	}
 
+		// Act
+		appProps.setEnvQueueName("ENV_QUEUE_NAME_MODIFIED");
+		appProps.setEnvNameSpace("ENV_QUEUE_NS_MODIFIED");
+		appProps.setEnvsasKeyName("ENV_KEY_NAME_MODIFIED");
+		appProps.setEnvsasKey("ENV_KEY_VALUE_MODIFIED");
+
+		// Assert
+		assertEquals("ENV_QUEUE_NAME_MODIFIED", appProps.getEnvQueueName());
+		assertEquals("ENV_QUEUE_NS_MODIFIED", appProps.getEnvNameSpace());
+		assertEquals("ENV_KEY_NAME_MODIFIED", appProps.getEnvsasKeyName());
+		assertEquals("ENV_KEY_VALUE_MODIFIED", appProps.getEnvsasKey());
+	}
 }
