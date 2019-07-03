48aecabbe213926c3ff6f0b9db31d14878fe78c3
Fernando Antivero <v-fean@microsoft.com>
2019-07-03 17:48:09 +0000

    Merged PR 819: Merged PR 809: drone utilization import for Cosmos DB
    drone utilization import for Cosmos DB
    
    drone utilization import for Cosmos DB
    
    a new console app that allows to bulk import data for DroneScheduler app.
    Specifically DroneUtilization documents with the following structure.
    It will be possible to pick out between deliveryid or ownerid as partitionkey
    value by switching true to false the --flattenPartitionKey arg
    
      - prescribe two collection creations with ~14 GB data each
      - first collection (utilization-cp) use a flatten partition key which
        happens to be 4X less efficient in terms of RU than the second one (utlization-sp). It is because cross partition vs    single partition query performance.
    
    ```json
    {
      "id": "d0003054570",
      "partitionKey": "<flatten-true: d0003054570 | flatten-false: o00042>",
      "type": "InternalDroneUtilization",
      "ownerId": "o00042",
      "year": "1969",
      "month": "07",
      "travelledMiles": 466.977477407538,
      "assignedHours": 5.00024804519501
    }
    ```
    
    solved: #9228
    
    Related work items: #9228

---
+++ src/shipping/dronescheduler/Fabrikam.DroneDelivery.DroneSchedulerService.BulkImport/Fabrikam.DroneDelivery.DroneSchedulerService.BulkImport/ImportConfiguration.cs
@@ -0,0 +1,39 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+using System;
+
+namespace Fabrikam.DroneDelivery.DroneSchedulerService.BulkImport
+{
+    internal class ImportConfiguration
+    {
+        public string EndpointUrl { get; set; }
+
+        public string AuthorizationKey { get; set; }
+
+        public string DatabaseName { get; set; }
+
+        public string CollectionName { get; set; }
+
+        public string CollectionPartitionKey { get; set; }
+
+        public int CollectionThroughput { get; set; }
+
+        public int NumberOfBatches { get; set; }
+
+        public long NumberOfDocuments { get; set; }
+
+        public int NumberDocumentsPerPartitionExpFactor { get; set; }
+
+        public string DocumentTypeName { get; set; }
+
+        public bool FlattenPartitionKey { get; set; }
+
+        public long NumberOfDocumentsPerBatch()
+        {
+            return (long)Math.Floor(((double)NumberOfDocuments) / NumberOfBatches);
+        }
+    }
+}
