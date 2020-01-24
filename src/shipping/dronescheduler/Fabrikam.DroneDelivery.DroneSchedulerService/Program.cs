8befd6148108296ee00032f2c07c705222c9ef47
Fernando Antivero (CLARIUS CONSULTING SA) <fla@clariusconsulting.net>
2020-01-24 16:58:05 +0000

    Merged PR 914: migrate Fabrikam.DroneDelivery.DroneSchedulerService to ASP.NET Core 3.1
    - migrate Fabrikam.DroneDelivery.DroneSchedulerService to .NET Core 3.1
    - upgrade Swashbuckle AspNetCore to 5.0.0. For more information, https://github.com/dotnet/aspnetcore/issues/7220
    - migrate Fabrikam.DroneDelivery.DroneSchedulerService.Tests to .NET Core 3.1
    - migrate Fabrikam.DroneDelivery.DroneSchedulerService.BulkImport to .NET Core 3.1
    
    solved: #160882

--- src/shipping/dronescheduler/Fabrikam.DroneDelivery.DroneSchedulerService/Program.cs
+++ src/shipping/dronescheduler/Fabrikam.DroneDelivery.DroneSchedulerService/Program.cs
@@ -3,10 +3,10 @@
 //  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
 // ------------------------------------------------------------
 
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Serilog;
 
@@ -16,31 +16,34 @@ namespace Fabrikam.DroneDelivery.DroneSchedulerService
     {
         public static void Main(string[] args)
         {
-            var host = CreateWebHostBuilder(args).Build();
+            var host = CreateHostBuilder(args).Build();
             var logger = host.Services.GetRequiredService<ILogger<Program>>();
             logger.LogInformation("Fabrikam Dronescheduler Service is starting.");
 
             host.Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .ConfigureAppConfiguration(configurationBuilder =>
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    var buildConfig = configurationBuilder.Build();
+                    webBuilder
+                        .UseStartup<Startup>()
+                        .ConfigureAppConfiguration(configurationBuilder =>
+                        {
+                            var buildConfig = configurationBuilder.Build();
 
-                    if (buildConfig["KEY_VAULT_URI"] is var keyVaultUri && !string.IsNullOrEmpty(keyVaultUri))
-                    {
-                        configurationBuilder.AddAzureKeyVault(keyVaultUri);
-                    }
-                })
-                .ConfigureLogging((hostingContext, loggingBuilder) =>
-                {
-                    loggingBuilder.AddApplicationInsights();
-                    loggingBuilder.AddSerilog(dispose: true);
-                })
-                .UseUrls("http://0.0.0.0:8080");
-
+                            if (buildConfig["KEY_VAULT_URI"] is var keyVaultUri && !string.IsNullOrEmpty(keyVaultUri))
+                            {
+                                configurationBuilder.AddAzureKeyVault(keyVaultUri);
+                            }
+                        })
+                        .ConfigureLogging((hostingContext, loggingBuilder) =>
+                        {
+                            loggingBuilder.AddApplicationInsights();
+                            loggingBuilder.AddSerilog(dispose: true);
+                        })
+                        .UseUrls("http://0.0.0.0:8080");
+                });
     }
 }
